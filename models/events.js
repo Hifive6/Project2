8d55ec94a5bd1fe5a115a9428cd90883496fde69
gpoell <gpoell.gp@gmail.com>
2018-10-15 19:33:46 -0400

    updated to create tables in db

---
+++ models/events.js
@@ -0,0 +1,44 @@
+module.exports = function(sequelize, DataTypes) {
+    var Events = sequelize.define("Events", {
+      // Giving the Author model a name of type STRING
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }  
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }  
+      },
+      pin: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }  
+      },
+      time: {
+        type: DataTypes.INTEGER,  
+        allowNull: false,
+        validate: {
+          len: [1]
+        }  
+      }
+    });
+  
+    Events.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Events.hasMany(models.Items, {
+        // onDelete: "cascade"
+      });
+    };
+  
+    return Events;
+  };
+  
