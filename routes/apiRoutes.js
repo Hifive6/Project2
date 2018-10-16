007358d7f61d151f9bb7e51a5161de4f94c9cf5a
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-10-15 22:41:35 -0400

    created addevent.js and connected event to post in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,8 +9,9 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/newEvent", function(req, res) {
+    console.log(req.body);
+    db.Events.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
