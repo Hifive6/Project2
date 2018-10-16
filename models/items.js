c3028f3e2c89b0839e1600a61595c6e82fa0af57
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-10-15 20:35:56 -0400

    adding mysql and password

--- models/items.js
+++ models/items.js
@@ -1,24 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
-    var Items = sequelize.define("Items", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
+  var Items = sequelize.define("Items", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Items.associate = function(models) {
+    // We're saying that a Items should belong to an Author
+    // A Items can't be created without an Author due to the foreign key constraint
+    Items.belongsTo(models.Events, {
+      foreignKey: {
+        allowNull: false
       }
     });
-  
-    Items.associate = function(models) {
-      // We're saying that a Items should belong to an Author
-      // A Items can't be created without an Author due to the foreign key constraint
-      Items.belongsTo(models.Events, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    return Items;
   };
-  
+
+  return Items;
+};
