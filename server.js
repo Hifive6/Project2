cc8abc0d059ded2084e63e5737931db329430a97
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-10-24 18:06:30 -0400

    trying heroku

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var connection = require("./config/connection.js")
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
